cd216bf2a7eecd14bea2160f8b492e1ea24c9e71
Programsko Inzenjerstvo <pi_foi@outlook.com>
2023-04-25 14:45:30 +0200

    Dorađena forma za prijavu

---
+++ EvaluationManagerG09/frmLogin.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace EvaluationManagerG09 {
+    partial class frmLogin {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.KorisnickoIme = new System.Windows.Forms.Label();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.Lozinka = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Location = new System.Drawing.Point(239, 110);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(129, 20);
+            this.txtUsername.TabIndex = 0;
+            this.txtUsername.TextChanged += new System.EventHandler(this.txtUsername_TextChanged);
+            // 
+            // KorisnickoIme
+            // 
+            this.KorisnickoIme.AutoSize = true;
+            this.KorisnickoIme.Location = new System.Drawing.Point(207, 80);
+            this.KorisnickoIme.Name = "KorisnickoIme";
+            this.KorisnickoIme.Size = new System.Drawing.Size(75, 13);
+            this.KorisnickoIme.TabIndex = 1;
+            this.KorisnickoIme.Text = "Korisničko ime";
+            this.KorisnickoIme.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(398, 252);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(97, 25);
+            this.btnLogin.TabIndex = 2;
+            this.btnLogin.Text = "Prijava";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            // 
+            // Lozinka
+            // 
+            this.Lozinka.AutoSize = true;
+            this.Lozinka.Location = new System.Drawing.Point(208, 155);
+            this.Lozinka.Name = "Lozinka";
+            this.Lozinka.Size = new System.Drawing.Size(44, 13);
+            this.Lozinka.TabIndex = 3;
+            this.Lozinka.Text = "Lozinka";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(240, 187);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(128, 20);
+            this.txtPassword.TabIndex = 4;
+            this.txtPassword.UseSystemPasswordChar = true;
+            // 
+            // frmLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(609, 315);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.Lozinka);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.KorisnickoIme);
+            this.Controls.Add(this.txtUsername);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmLogin";
+            this.Text = "Prijava";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.Label KorisnickoIme;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Label Lozinka;
+        private System.Windows.Forms.TextBox txtPassword;
+    }
+}
+
